1823c360e0c3e483b84da6b15eb9c9dd26995a4d
chengshen <cschengshen@gmail.com>
2025-08-19 12:54:56 +0800

    update

--- wallet-track/src/App.vue
+++ wallet-track/src/App.vue
@@ -4,12 +4,13 @@
       <div class="tab-group">
         <div
           class="tab-item"
-          :class="{ active: fromActive === platform.name }"
+          :class="{ yellow: fromActive === platform.name }"
           v-for="platform in platforms"
           @click="fromActive = platform.name"
         >
           {{ platform.name }}
         </div>
+        <div class="tab-item" @click="from = ''">清空</div>
       </div>
       <div class="tab-main">
         <textarea name="from" id="from" v-model="from"></textarea>
@@ -19,22 +20,34 @@
       <div class="tab-group">
         <div
           class="tab-item"
-          :class="{ active: toActive === platform.name }"
+          :class="{ yellow: toActive === platform.name }"
           v-for="platform in platforms"
           @click="toActive = platform.name"
         >
           {{ platform.name }}
         </div>
+        <div class="tab-item" :class="{ red: reverse }" @click="reverse = !reverse">反序</div>
+        <div class="tab-item" @click="copy">复制</div>
+        <div class="info" v-if="count">共 {{ count }} 项</div>
+        <div class="info" :class="{ green: green }">{{ info }}</div>
       </div>
       <div class="tab-main">
-        <textarea name="to" id="to" v-model="to"></textarea>
+        <textarea
+          name="to"
+          id="to"
+          v-model="to"
+          ref="textarea"
+          :class="{ copied: copied }"
+          @click="copy"
+          readonly
+        ></textarea>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, type ComputedRef, ref } from 'vue'
 import {
   fromAxiom,
   fromBullx,
@@ -48,9 +61,9 @@ import {
 
 const platforms = [
   {
-    name: 'Bullx',
-    from: fromBullx,
-    to: toBullx
+    name: 'GMGN',
+    from: fromGmgn,
+    to: toGmgn
   },
   {
     name: 'Axiom',
@@ -58,32 +71,66 @@ const platforms = [
     to: toAxiom
   },
   {
-    name: 'GMGN',
-    from: fromGmgn,
-    to: toGmgn
-  },
-  {
     name: 'OKX',
     from: fromOkx,
     to: toOkx
+  },
+  {
+    name: 'Bullx',
+    from: fromBullx,
+    to: toBullx
   }
 ]
 
-const fromActive = ref('OKX')
-const toActive = ref('Axiom')
+const fromActive = ref(platforms[0].name)
+const toActive = ref(platforms[1].name)
 
 const from = ref('')
+const reverse = ref(false)
+const info = ref('')
+const green = ref(false)
+const textarea = ref<HTMLTextAreaElement>()
+const copied = ref(false)
+const count = ref(0)
 
-const to = computed(() => {
+const to: ComputedRef<string> = computed((): string => {
+  count.value = 0
   if (!from.value) {
-    return null
+    return ''
   }
   const nowFrom = platforms.find(p => p.name === fromActive.value)
   const nowTo = platforms.find(p => p.name === toActive.value)
-  if (nowFrom === undefined || nowTo === undefined) return
-  const mid = nowFrom.from(from.value)
-  return nowTo?.to(mid)
+  if (nowFrom === undefined || nowTo === undefined) return ''
+  try {
+    const mid = nowFrom.from(from.value)
+    count.value = mid.length
+    reverse.value && mid.reverse()
+    return nowTo?.to(mid) || ''
+  } catch (err) {
+    console.error(err)
+    return ''
+  }
 })
+
+const copy = async () => {
+  if (!to.value) {
+    return
+  }
+  ;(textarea.value as HTMLTextAreaElement).select()
+  try {
+    await navigator.clipboard.writeText(to.value)
+    setTimeout(() => {
+      copied.value = false
+      info.value = ''
+      green.value = false
+    }, 3000)
+    copied.value = true
+    info.value = '内容已复制到剪贴板'
+    green.value = true
+  } catch (err) {
+    console.error('复制失败:', err)
+  }
+}
 </script>
 
 <style scoped>
@@ -96,6 +143,7 @@ const to = computed(() => {
   flex-direction: column;
   gap: 10px;
   align-items: center;
+  justify-content: space-around;
 }
 
 .row {
@@ -131,7 +179,24 @@ textarea {
   height: 100%;
 }
 
+textarea.copied {
+  outline: 1px solid #4caf50;
+  border: 1px solid #4caf50;
+}
+
 .active {
   color: yellow;
+}
+
+.yellow {
+  color: yellow;
+}
+
+.red {
+  color: red;
+}
+
+.green {
+  color: green;
 }
 </style>
--- wallet-track/src/functions.ts
+++ wallet-track/src/functions.ts
@@ -1,54 +1,136 @@
 // @ts-nocheck
 
-import type { Common, Axiom } from './types.ts'
+import type { Common, Bullx, Axiom } from './types.ts'
 
 export const fromBullx = (params: string): Common[] => {
-  return []
+  const addressSet = new Set<string>()
+  const list = JSON.parse(params)
+  const isValid = list[0].hasOwnProperty('address')
+  if (!isValid) {
+    // return fromAxiom(params)
+    return []
+  }
+  return list
+    .map(
+      item =>
+        ({
+          name: item.name,
+          address: item.address,
+          emoji: item.emoji,
+          toast: false,
+          bubble: true,
+          feed: true,
+          groups: [],
+          sound: 'default'
+        }) as unknown as Common
+    )
+    .filter(item => {
+      if (addressSet.has(item.address)) {
+        return false
+      }
+      addressSet.add(item.address)
+      return true
+    })
 }
 export const toBullx = (params: Common[]): string => {
-  return ''
+  return JSON.stringify(
+    params.map(
+      param =>
+        ({
+          name: param.name,
+          address: param.address,
+          emoji: param.emoji
+        }) as unknown as Bullx
+    ),
+    null,
+    2
+  )
 }
+
+export const fromGmgn = fromBullx
+export const toGmgn = toBullx
 
 export const fromAxiom = (params: string): Common[] => {
-  return []
+  const addressSet = new Set<string>()
+  const list = JSON.parse(params)
+  const isValid = list[0].hasOwnProperty('trackedWalletAddress')
+  if (!isValid) {
+    // return fromBullx(params)
+    return []
+  }
+  return list
+    .map(
+      item =>
+        ({
+          name: item.name,
+          address: item.trackedWalletAddress,
+          emoji: item.emoji,
+          toast: item.alertsOnToast,
+          bubble: item.alertsOnBubble,
+          feed: item.alertsOnFeed,
+          groups: item.groups,
+          sound: item.sound
+        }) as unknown as Common
+    )
+    .filter(item => {
+      if (addressSet.has(item.address)) {
+        return false
+      }
+      addressSet.add(item.address)
+      return true
+    })
 }
 export const toAxiom = (params: Common[]): string => {
-  return JSON.stringify(params
-    .map(param => {
-      return {
-        trackedWalletAddress: param.address,
-        name: param.name,
-        emoji: param.emoji,
-        alertsOnToast: param.toast,
-        alertsOnBubble: param.bubble,
-        alertsOnFeed: param.feed,
-        groups: param.groups,
-        sound: param.sound
-      } as unknown as Axiom
-    }), null, 2)
-}
-
-export const fromGmgn = (params: string): Common[] => {
-  return []
-}
-export const toGmgn = (params: Common[]): string => {
-  return ''
+  return JSON.stringify(
+    params.map(
+      param =>
+        ({
+          trackedWalletAddress: param.address,
+          name: param.name,
+          emoji: param.emoji,
+          alertsOnToast: param.toast,
+          alertsOnBubble: param.bubble,
+          alertsOnFeed: param.feed,
+          groups: param.groups,
+          sound: param.sound
+        }) as unknown as Axiom
+    ),
+    null,
+    2
+  )
 }
 
 export const fromOkx = (params: string): Common[] => {
-  return params.split('\n').map(address => {
-    return {
-      name: address.slice(0, 5),
-      address: address,
-      emoji: null,
-      toast: false,
-      bubble: true,
-      feed: true,
-      groups: [],
-      sound: 'default'
-    } as unknown as Common
-  })
+  const addressSet = new Set<string>()
+  try {
+    // 如果入参是一个合法json，那么就不是 okx 的风格
+    JSON.parse(params)
+    // return fromAxiom(params)
+    return []
+  } catch (_) {}
+
+  return params
+    .split('\n')
+    .filter(line => line.trim() !== '') // 过滤空行
+    .map(
+      address =>
+        ({
+          name: address.slice(0, 5),
+          address: address.trim(), // 去除前后空格
+          emoji: null,
+          toast: false,
+          bubble: true,
+          feed: true,
+          groups: [],
+          sound: 'default'
+        }) as unknown as Common
+    )
+    .filter(item => {
+      if (addressSet.has(item.address)) {
+        return false
+      }
+      addressSet.add(item.address)
+      return true
+    })
 }
-export const toOkx = (params: Common[]): string => {
-  return ''
-}
+export const toOkx = toBullx
