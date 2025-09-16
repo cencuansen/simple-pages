0d9e889962b810a63552f3809fe64c0648c620e4
chengshen <cschengshen@gmail.com>
2025-09-17 00:15:35 +0800

    update japanese

--- japanese/src/views/lesson/LessonHeader.vue
+++ japanese/src/views/lesson/LessonHeader.vue
@@ -5,72 +5,48 @@
         上一课
       </el-button>
       <LessonSelect v-model="currentIndex" :clearable="false" fit-input-width />
-      <el-button :disabled="!hasNext" @click="goNext">
-        下一课
-      </el-button>
+      <el-button :disabled="!hasNext" @click="goNext"> 下一课 </el-button>
     </div>
     <div class="function-group">
       <el-button
         :type="settingStore.allTranslate ? 'primary' : ''"
-
-        circle
         title="翻译"
         v-if="settingStore.translate"
         @click="settingStore.setAllTranslate(!settingStore.allTranslate)"
       >
-        译
+        翻译
       </el-button>
       <el-button
         :type="settingStore.furigana ? 'primary' : ''"
-
-        circle
         title="注音"
         @click="settingStore.furiganaToggle"
       >
-        注
+        注音
       </el-button>
       <el-button
         :type="settingStore.wordLink ? 'primary' : ''"
-
-        circle
         title="单词跳转"
         @click="settingStore.wordLinkToggle"
       >
-        跳
+        跳转
+      </el-button>
+      <el-button :type="''" title="搜索" @click="setDialog(!dialog)">
+        搜索
+      </el-button>
+      <el-button title="全屏" v-if="!fullscreen" @click="toggleFullscreen">
+        全屏
       </el-button>
       <el-button
         :type="''"
-
-        circle
-        title="搜索"
-        @click="setDialog(!dialog)"
-      >
-        搜
-      </el-button>
-      <el-button
-
-        circle
-        title="全屏"
-        v-if="!fullscreen"
-        @click="toggleFullscreen"
-      >
-        全
-      </el-button>
-      <el-button
-        :type="''"
-
-        circle
         title="播放"
         :disabled="isReading"
         v-if="hasAudio && settingStore.audioSpeak"
         @click="playAudio({ id: lessonAudio || '', text: lessonAudio || '' })"
       >
-        读
+        播放
       </el-button>
       <el-button
         :type="''"
-
-        circle
         v-if="isReading"
         title="停止播放"
         @click="
@@ -80,7 +56,7 @@
           }
         "
       >
-        停
+        停止
       </el-button>
     </div>
   </div>
@@ -150,6 +126,13 @@ const toggleFullscreen = (newStatus: boolean | null = null) => {
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100px;
+  width: 100%;
+  max-width: var(--content-max-width);
+  overflow-x: scroll;
+  overflow-y: hidden;
+}
+
+.function-group::-webkit-scrollbar {
+  display: none;
 }
 </style>
