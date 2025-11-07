4d0c37ac80a0cd598aea8ae99a930216312f5cf0
chengshen <cschengshen@gmail.com>
2025-11-07 09:07:39 +0800

    update japanese

--- japanese/src/utils/dictionary.ts
+++ japanese/src/utils/dictionary.ts
@@ -8,5 +8,14 @@ export const toUrl = (
   if (!word || !dict || !dict.url) {
     return void 0
   }
+
+  // 去除开头和结尾的 ~、～
+  word = word.replace(/^[~～]|[~～]$/g, '')
+
+  // 去除词尾的 する
+  if (word.endsWith('する')) {
+    word = word.slice(0, -2)
+  }
+
   return dict.url.replace(placeholder, word)
 }
